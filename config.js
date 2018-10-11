0a689f56b31fcff989c361fa5d91999fc8cf92bf
Shubham <arya.xoert@gmail.com>
2018-10-11 16:51:41 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key: "KrRMnFBqNadScHgbu01qJ7RmR",
+  consumer_secret: "vY5kGMZsbUkzp2anacWXvSJMmOxwAz2VOVjAFOPCrjmz50Eh7v",
+  // app_only_auth: true,
+  access_token: "1048562508234027008-UPI918oBXRwVLvR8h68Uh5GBjTWdhu",
+  access_token_secret: "YeGUDM8yzsl0XCRZHQwXNR3Fkb34EDbKHTkpkskwHenGg"
+};
